d0ba7fe9df9c3240bf56127e0933ac85a0db0337
Vuizur <Vuizur@users.noreply.github.com>
2021-12-10 12:05:59 +0100

    Remove test code

--- helper_functions.py
+++ helper_functions.py
@@ -52,8 +52,3 @@ def has_cyrillic_letters(s: str):
 #Wiktionary data is a bit buggy
 def remove_weird_characters_for_alternative_canonical(s: str):
     return(s.replace("^*", ""))
-
-gloss_string = "Pronunciation spelling of abuelo"
-standard_form = gloss_string[26:].replace(" (", ";").split(";", 1)[0]
-
-print(unidecode("niño"))

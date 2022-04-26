b218895cab6c89555d36c57ebd86ee24436cb605
Vuizur <Vuizur@users.noreply.github.com>
2022-04-26 17:09:03 +0200

    Small function extracting relevant data

--- various_tools/kaikki_raw_extract_test.py
+++ various_tools/kaikki_raw_extract_test.py
@@ -1,7 +1,10 @@
 import json
-with open("D:/raw-wiktextract-data.json", "r", encoding="utf-8") as f:
-    for line in f:
-        obj = json.loads(line)
-        if "word" in obj and obj["word"] == "писать":
-            print(obj)
-            print("#\n")
+
+def extract_russian_part():
+    with open("D:/raw-wiktextract-data.json", "r", encoding="utf-8") as f, \
+         open("D:/raw-wiktextract-data_russian.json", "w", encoding="utf-8") as out:
+    
+        for line in f:
+            obj = json.loads(line)
+            if "lang" in obj and obj["lang"] == "Russian":
+                out.write(line)

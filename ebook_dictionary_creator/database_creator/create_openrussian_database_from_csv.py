4d5eb1260ff45c6097f06e2ac610311311e263b0
Vuizur <Vuizur@users.noreply.github.com>
2022-09-09 11:35:07 +0200

    Fix openrussian database creation

--- ebook_dictionary_creator/database_creator/create_openrussian_database_from_csv.py
+++ ebook_dictionary_creator/database_creator/create_openrussian_database_from_csv.py
@@ -14,20 +14,20 @@ def create_openrussian_database(openrussian_database_path):
 
     conn = sqlite3.connect(openrussian_database_path)
     cur = conn.cursor()
-    adj_path = "data/openrussian-csvs/russian3 - adjectives.csv"
+    adj_path = "russian3 - adjectives.csv"
     # Load with pkg_resources
 
-    conj_path = "openrussian-csvs/russian3 - conjugations.csv"
-    decl_path = "openrussian-csvs/russian3 - declensions.csv"
-    exp_w_path = "openrussian-csvs/russian3 - expressions_words.csv"
-    noun_path = "openrussian-csvs/russian3 - nouns.csv"
-    sent_w_path = "openrussian-csvs/russian3 - sentences_words.csv"
-    sent_path = "openrussian-csvs/russian3 - sentences.csv"
-    transl_path = "openrussian-csvs/russian3 - translations.csv"
-    verb_path = "openrussian-csvs/russian3 - verbs.csv"
-    word_f_path = "openrussian-csvs/russian3 - words_forms.csv"
-    words_rels_path = "openrussian-csvs/russian3 - words_rels.csv"
-    words_path = "openrussian-csvs/russian3 - words.csv"
+    conj_path = "russian3 - conjugations.csv"
+    decl_path = "russian3 - declensions.csv"
+    exp_w_path = "russian3 - expressions_words.csv"
+    noun_path = "russian3 - nouns.csv"
+    sent_w_path = "russian3 - sentences_words.csv"
+    sent_path = "russian3 - sentences.csv"
+    transl_path = "russian3 - translations.csv"
+    verb_path = "russian3 - verbs.csv"
+    word_f_path = "russian3 - words_forms.csv"
+    words_rels_path = "russian3 - words_rels.csv"
+    words_path = "russian3 - words.csv"
 
     cur.execute("CREATE TABLE IF NOT EXISTS adjectives (word_id INTEGER NOT NULL PRIMARY KEY,incomparable INTEGER,comparative VARCHAR,superlative VARCHAR,short_m VARCHAR,short_f VARCHAR,short_n VARCHAR,short_pl VARCHAR,decl_m_id INTEGER,decl_f_id INTEGER,decl_n_id INTEGER,decl_pl_id INTEGER)")
 
@@ -45,7 +45,7 @@ def create_openrussian_database(openrussian_database_path):
         
 
     cur.execute("CREATE TABLE IF NOT EXISTS conjugations (id INTEGER NOT NULL PRIMARY KEY,word_id INTEGER,sg1 VARCHAR,sg2 VARCHAR, sg3 VARCHAR, pl1 VARCHAR, pl2 VARCAR, pl3 VARCHAR)")
-    with open(conj_path, encoding="utf-8") as conjugations:
+    with pkg_resources.open_text(data, conj_path) as conjugations:
         csvData = csv.reader(conjugations, delimiter=",")
         cur.execute('BEGIN TRANSACTION')
         print(next(csvData))
@@ -58,7 +58,7 @@ def create_openrussian_database(openrussian_database_path):
 
     cur.execute("CREATE TABLE IF NOT EXISTS declensions (id INTEGER NOR NULL PRIMARY KEY,word_id INTEGER,nom VARCHAR,gen VARCHAR, dat VARCHAR, acc VARCHAR, inst VARCAR, prep VARCHAR)")
 
-    with open(decl_path, encoding="utf-8") as declensions:
+    with pkg_resources.open_text(data, decl_path) as declensions:
         csvData = csv.reader(declensions, delimiter=",")
         cur.execute('BEGIN TRANSACTION')
         print(next(csvData))
@@ -71,7 +71,7 @@ def create_openrussian_database(openrussian_database_path):
     
     cur.execute("CREATE TABLE IF NOT EXISTS expression_words (id INTEGER NOT NULL PRIMARY KEY,expression_id INTEGER,referenced_word_id INTEGER, start INTEGER, length INTEGER)")
 
-    with open(exp_w_path, encoding="utf-8") as expression_words:
+    with pkg_resources.open_text(data, exp_w_path) as expression_words:
         csvData = csv.reader(expression_words, delimiter=",")
         cur.execute('BEGIN TRANSACTION')
         print(next(csvData))
@@ -84,7 +84,7 @@ def create_openrussian_database(openrussian_database_path):
 
     cur.execute("CREATE TABLE IF NOT EXISTS nouns (word_id INTEGER NOT NULL PRIMARY KEY,gender TEXT,partner VARCHAR, animate INTEGER, indeclinable INTEGER, sg_only INTEGER, pl_only INTEGER, decl_sg_id INTEGER, decl_pl_id INTEGER)")
 
-    with open(noun_path, encoding="utf-8") as nouns:
+    with pkg_resources.open_text(data, noun_path) as nouns:
         csvData = csv.reader(nouns, delimiter=",")
         cur.execute('BEGIN TRANSACTION')
         print(next(csvData))
@@ -97,7 +97,7 @@ def create_openrussian_database(openrussian_database_path):
 
     cur.execute("CREATE TABLE IF NOT EXISTS sentences_words (id INTEGER NOT NULL PRIMARY KEY,sentence_id INTEGER,word_id INTEGER, start INTEGER, length INTEGER)")
 
-    with open(sent_w_path, encoding="utf-8") as sentences_words:
+    with pkg_resources.open_text(data, sent_w_path) as sentences_words:
         csvData = csv.reader(sentences_words, delimiter=",")
         cur.execute('BEGIN TRANSACTION')
         print(next(csvData))
@@ -110,7 +110,7 @@ def create_openrussian_database(openrussian_database_path):
 
     cur.execute("CREATE TABLE IF NOT EXISTS sentences (id INTEGER NOT NULL PRIMARY KEY,ru VARCHAR,lang VARCHAR, tl VARCHAR, tatobae_key INTEGER, source_url VARCHAR, disabled INTEGER, locked INTEGER, level TEXT, audio_url VARCHAR, audio_attribution_url VARCHAR, audio_attribution_name VARCHAR)")
 
-    with open(sent_path, encoding="utf-8") as sentences:
+    with pkg_resources.open_text(data, sent_path) as sentences:
         csvData = csv.reader(sentences, delimiter=",")
         cur.execute('BEGIN TRANSACTION')
         print(next(csvData))
@@ -123,7 +123,7 @@ def create_openrussian_database(openrussian_database_path):
 
     cur.execute("CREATE TABLE IF NOT EXISTS translations (id INTEGER NOT NULL PRIMARY KEY,lang TEXT,word_id INTEGER, position INTEGER, tl VARCHAR, example_ru VARCHAR, example_tl VARCHAR, info VARCHAR)")
 
-    with open(transl_path, encoding="utf-8") as translations:
+    with pkg_resources.open_text(data, transl_path) as translations:
         csvData = csv.reader(translations, delimiter=",")
         cur.execute('BEGIN TRANSACTION')
         print(next(csvData))
@@ -136,7 +136,7 @@ def create_openrussian_database(openrussian_database_path):
 
     cur.execute("CREATE TABLE IF NOT EXISTS verbs (word_id INTEGER NOT NULL PRIMARY KEY, aspect TEXT, partner VARCHAR, imperative_sg VARCHAR, imperative_pl VARCHAR, past_m VARCHAR, past_f VARCHAR, past_n VARCHAR, past_pl VARCHAR, presfut_conj_id INTEGER, participle_active_present_id INTEGER,participle_active_past_id INTEGER,participle_passive_present_id INTEGER,participle_passive_past_id INTEGER)")
 
-    with open(verb_path, encoding="utf-8") as verbs:
+    with pkg_resources.open_text(data, verb_path) as verbs:
         csvData = csv.reader(verbs, delimiter=",")
         cur.execute('BEGIN TRANSACTION')
         print(next(csvData))
@@ -151,7 +151,7 @@ def create_openrussian_database(openrussian_database_path):
 
     cur.execute("CREATE TABLE IF NOT EXISTS words (id INTEGER NOT NULL PRIMARY KEY, position INTEGER, bare VARCHAR, accented VARCHAR, derived_from_word_id INTEGER, rank INTEGER, disabled INTEGER, usage_en TEXT, usage_de TEXT, number_value INTEGER, type TEXT, level TEXT, created_at TEXT)")
 
-    with open(words_path, encoding="utf-8") as words:
+    with pkg_resources.open_text(data, words_path) as words:
         csvData = csv.reader(words, delimiter=",")
         cur.execute('BEGIN TRANSACTION')
         print(next(csvData))
@@ -164,7 +164,7 @@ def create_openrussian_database(openrussian_database_path):
 
     cur.execute("CREATE TABLE IF NOT EXISTS words_rels (id INTEGER NOT NULL PRIMARY KEY, word_id INTEGER, rel_word_id INTEGER, relation TEXT)")
 
-    with open(words_rels_path, encoding="utf-8") as words_rels:
+    with pkg_resources.open_text(data, words_rels_path) as words_rels:
         csvData = csv.reader(words_rels, delimiter=",")
         cur.execute('BEGIN TRANSACTION')
         print(next(csvData))
---
+++ ebook_dictionary_creator/examples/test_openrussian.py
@@ -0,0 +1,5 @@
+from ebook_dictionary_creator.database_creator.create_openrussian_database_from_csv import create_openrussian_database
+
+
+if __name__ == "__main__":
+    create_openrussian_database("openrussian-test.db")
